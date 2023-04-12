c8deaf31ce55fd20bd843b8357c8aff76696c889
Matt Fowler <mbfowler@gmail.com>
2023-04-12 19:29:09 -0400

    Address review comments

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 var lockButton = document.querySelector('.main-display');
 
+window.addEventListener('load', getNewHexes);
+
 lockButton.addEventListener('click', function(event) {
     if (event.target.className === 'lock-box') {
         lockToggle(event.target);
     }
 });
 
-function getRandomHex() {
-    return (Math.floor(Math.random() * 16777216).toString(16).padStart(6, 0));
+newPaletteButtonSection.addEventListener('click', function(event) {
+    if(event.target.classList.contains('button-box-l') || event.target.id === 'new-palette' || event.target.classList.contains('button-box-r')) {
+        getNewHexes();
+    }
+});
+
+function getNewHexes() {
+    currentHexes = [];
+    var newColor;
+    for(i = 0; i < mainColorBoxes.length; i++) {
+        newColor = getRandomHex().toUpperCase();
+        mainColorBoxes[i].firstElementChild.style.backgroundColor = `#${newColor}`;
+        mainColorBoxes[i].lastElementChild.innerText = `#${newColor}`;
+    }
 }
 
 function lockToggle(event) {
